2fa541c6bc0c99bbd692ac4c0925ad322a79a9d7
nasse <nasserwahbeh198@gmail.com>
2022-02-21 13:22:40 -0500

    Final

--- app/src/main/java/com/example/catalog2/Activities/MainActivity.java
+++ app/src/main/java/com/example/catalog2/Activities/MainActivity.java
@@ -73,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
        getMenuInflater().inflate(R.menu.recherche, menu);
        MenuItem menuItem = menu.findItem(R.id.action_search);
         SearchView searchView =(SearchView) menuItem.getActionView();
-        searchView.setQueryHint("Write the league number between 1 - 50");
+        searchView.setQueryHint("League number between 1-50");
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
